3bb2e1d614d7fd6caed6f1bfa7e222dc34b3fc0e
dregelyimi <dregely.imi@gmail.com>
2021-03-08 11:20:46 +0100

    2. feladat felkesz (megint)

--- src/szamologep/Szamologep.java
+++ src/szamologep/Szamologep.java
@@ -1,10 +1,54 @@
 package szamologep;
 
+import java.util.ArrayList;
+
 public class Szamologep {
 
 
     public static void main(String[] args) {
         System.out.println("Hello there");
+        
+        ArrayList<String> segedverem = new ArrayList<>();
+        ArrayList<String> lengyelForm = new ArrayList<>();
+        //2+6*(9-5) | 2695-*+ | Elvárt eredmény: 26
+        lengyelForm.add("2");
+        lengyelForm.add("6");
+        lengyelForm.add("9");
+        lengyelForm.add("5");
+        lengyelForm.add("-");
+        lengyelForm.add("*");
+        lengyelForm.add("+");
+        segedverem.clear();
+        
+        for (int i = 0; i < lengyelForm.size(); i++) {
+            
+        }
+        
+        
+        
+        
+        
+     
+        
+        
+        
+        
+        
+        
+//        Verem.ürít
+//Ciklus amíg (nem üres lengyel_forma)
+//	elem: lengyel_forma.elso_elem
+//	ha (elem típusa adat)
+//		verembe(elem)
+//	 ha (elem típusa művelet)
+//		érték1=veremből()
+//		érték2=veremből()
+//		eredmény=kiszámol(érték2, művelet, érték1) (figyelni kell, érték2-érték1)
+//		verembe(eredmény)
+//	elágazás vége
+//Ciklus vége
+//Végeredmény=veremből()
+
     }
     
 }
