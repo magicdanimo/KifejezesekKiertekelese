bbcea18e9bec75e32eb99ce5b845d159ef111496
NadasyB <nadasy.bendeguz@gmail.com>
2021-03-09 15:35:19 +0100

    javitott lenygelforma

--- src/szamologep/Szamologep.java
+++ src/szamologep/Szamologep.java
@@ -9,25 +9,30 @@ public class Szamologep {
         System.out.println("Hello there");
         ArrayList<String> segedverem = new ArrayList<>();
         ArrayList<String> lengyelForm = new ArrayList<>();
-        //2+6*(9-5) | 2695-*+ | Elvárt eredmény: 26
-        lengyelForm.add("2");
-        lengyelForm.add("6");
-        lengyelForm.add("9");
-        lengyelForm.add("5");
-        lengyelForm.add("-");
-        lengyelForm.add("*");
-        lengyelForm.add("+");
-        segedverem.clear();
-
-        for (int i = 0; i < lengyelForm.size(); i++) {
-
-        }
+//        //2+6*(9-5) | 2695-*+ | Elvárt eredmény: 26
+//        lengyelForm.add("2");
+//        lengyelForm.add("6");
+//        lengyelForm.add("9");
+//        lengyelForm.add("5");
+//        lengyelForm.add("-");
+//        lengyelForm.add("*");
+//        lengyelForm.add("+");
+//        segedverem.clear();
+//
+//        for (int i = 0; i < lengyelForm.size(); i++) {
+//
+//        }
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Adj meg egy matematikai kifejezést: ");
         String matematikai = sc.nextLine();
 
-        lengyelFormAlakit(matematikai);
+        lengyelForm = lengyelFormAlakit(matematikai);
+        
+        for (String akt : lengyelForm) {
+            System.out.print(akt);
+        }
+        System.out.println("");
     }
 
     private static ArrayList lengyelFormAlakit(String matematikai) {
@@ -48,17 +53,51 @@ public class Szamologep {
                 if (verem.isEmpty()) {
                     verem.add(akt + "");
                 } else {
-                    //todo
+//                    String utolsoElem =
+                    while (!verem.isEmpty() /*&& todo*/) {
+                        String utolsoElem = verem.get(verem.size() - 1);
+                        if (utolsoElem.equals("(")) {
+                            verem.add(akt+"");
+                            break;
+//                            verem.remove(utolsoElem);
+                        } 
+                        else if(utolsoElem.equals("*") || utolsoElem.equals("/")) {
+                            lengyelform.add(utolsoElem);
+                            verem.remove(verem.size() - 1);
+                        } 
+                        else if ((utolsoElem.equals("+") || utolsoElem.equals("-")) && !(akt == '*' || akt == '/')) {
+                            lengyelform.add(utolsoElem);
+                            verem.remove(verem.size() - 1);
+                            
+                        }
+                        System.out.println(utolsoElem);
+                    }
+//                    verem.add(akt + "");
+                   
                 }
             }
             if (akt == ')') {
-                verem.add(akt + "");
-                //todo
+//                verem.add(akt + "");
+                String kivettElem = "";
+                do {
+                    if (verem.size() > 0) {
+                        kivettElem = verem.get(verem.size() - 1);
+                        if (!kivettElem.equals("(")) {
+                            lengyelform.add(kivettElem);
+                            verem.remove(verem.size() - 1);
+                        }
+                    }    
+                    System.out.println(kivettElem);
+                    System.out.println(verem.size());
+                    
+                } while (!kivettElem.equals("("));
+                verem.remove(verem.size() - 1);
             }
 
         }
         while (!verem.isEmpty()) {
-            //todo
+            lengyelform.add(verem.get(verem.size() - 1));
+            verem.remove(verem.size() - 1);
         }
         return lengyelform;
     }
